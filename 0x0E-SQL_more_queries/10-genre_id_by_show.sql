8b90340a3eb88013249622e811bf34102fa6bfbb
Stanley <san.cathline@gmail.com>
2022-11-03 05:11:02 -0700

    more sql queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
-i-- List all shows in 'hbtn_0d_tvshows' that have at least one genre linked
+-- List all shows in 'hbtn_0d_tvshows' that have at least one genre linked
 -- Each record should display
 -- tv_shows.title, tv_show_genres.genre_id
 -- Results must be in ascending order by tv_shows.title and tv_show_genres.genre_id
