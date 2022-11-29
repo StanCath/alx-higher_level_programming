27d046e4cecb79eedb2fdfe8530040dcfbec4f0a
Stanley <san.cathline@gmail.com>
2022-11-29 03:38:41 -0800

    JavaScript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-
-if (process.argv.length < 4) {
-  console.log('0');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  const nums = [];
-  for (let i = 0; i < (process.argv.length - 2); i++) {
-    nums[i] = parseInt(process.argv[i + 2]);
-  }
-  nums.sort(function sortNum (a, b) { return a - b; });
-  console.log(nums);
-  console.log(nums[nums.length - 2]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
